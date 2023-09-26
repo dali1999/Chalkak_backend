38b631df8c8e974fd16d729660995b5bc65ea728
dali1999 <dali1999@naver.com>
2023-09-26 22:03:56 +0900

    chore: jsonwebtoken

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
 // const productRouter = require("./routes/products");
-// const authRouter = require("./routes/auth");
-// const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
 // const cartRouter = require("./routes/cart");
 // const orderRouter = require("./routes/order");
 const port = 3000;
@@ -19,8 +19,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // app.use("/api/products", productRouter);
-// app.use("/api/", authRouter);
-// app.use("/api/users", userRouter);
+app.use("/api/", authRouter);
+app.use("/api/users", userRouter);
 // app.use("/api/orders", orderRouter);
 // app.use("/api/carts", cartRouter);
 
