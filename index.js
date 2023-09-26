c672cf60bc93a1a816dc0cdd5281d9f54fe51b58
dali1999 <dali1999@naver.com>
2023-09-26 21:51:01 +0900

    chore: cryptoJS

--- index.js
+++ index.js
@@ -18,11 +18,11 @@ mongoose
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use("/api/products", productRouter);
-app.use("/api/", authRouter);
-app.use("/api/users", userRouter);
-app.use("/api/orders", orderRouter);
-app.use("/api/carts", cartRouter);
+// app.use("/api/products", productRouter);
+// app.use("/api/", authRouter);
+// app.use("/api/users", userRouter);
+// app.use("/api/orders", orderRouter);
+// app.use("/api/carts", cartRouter);
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
